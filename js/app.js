2fb9406966a438023c9eb4abb37a07bfd15e9049
Kummari, Venky (NIH/OD) [C] <venky.kummari@nih.gov>
2021-09-17 13:13:49 -0400

    Revision 3 - Removing console.log statements

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+// Get all the articles on the page
+const tennisarticles = document.querySelectorAll("article");
+
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("Hello - Page loaded")
     buildNavigationFromArticles();
 });
 
@@ -7,12 +9,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
  * Builds the navigation menu based on the articles available on the page
  */
 function buildNavigationFromArticles() {
-    // Get all the articles on the page
-    const tennisarticles = document.querySelectorAll("article");
     // Create navigationFragment which will hold all anchor elements
     const navigationFragment = document.createDocumentFragment();
     for (const tennisarticle of tennisarticles) {
-        console.log(tennisarticle.querySelector("h2").innerText);
         const tennisarticleheader = tennisarticle.querySelector("h2").innerText;
         // Create the navigation element and append to the fragment
         const navigationElement = document.createElement('a');
@@ -22,8 +21,6 @@ function buildNavigationFromArticles() {
         addOnClickEventToNaivgationItem(navigationElement);
         navigationFragment.appendChild(navigationElement);
     }
-    console.log("Navigation Fragment" + navigationFragment);
-    console.log(document.getElementById("navigationsid"));
     document.getElementById("navigationsid").appendChild(navigationFragment);
 
 }
@@ -33,11 +30,40 @@ function buildNavigationFromArticles() {
  * Assoicates onClick event to Navigation Item 
  */
 function addOnClickEventToNaivgationItem(navigationElement) {
-    console.log("Inside associating on Click event")
     navigationElement.addEventListener("click", function(event) {
         event.preventDefault();
-        console.log(navigationElement.innerText + " is clicked");
-        console.log(document.getElementById(navigationElement.innerText));
         document.getElementById(navigationElement.innerText).scrollIntoView(true);
     });
+}
+
+/**
+ * Associates scroll event to the document and highlights the section in view port
+ */
+document.addEventListener('scroll', function(e) {
+    t = setTimeout(function() {
+        const elementInViewPort = getElementInViewPort();
+        if (elementInViewPort != undefined) {
+            elementInViewPort.classList.add("sectionHighlight");
+        }
+
+    }, 300);
+});
+
+/**
+ * Fetches the current element in view port
+ */
+function getElementInViewPort() {
+    // Reset all elements backgroud color
+    for (const tennisarticle of tennisarticles) {
+        tennisarticle.classList.remove("sectionHighlight");
+    }
+    // Get the first element which is in the viewport
+    for (const tennisarticle of tennisarticles) {
+        const h2Element = tennisarticle.firstElementChild;
+        var bounding = h2Element.getBoundingClientRect();
+
+        if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+            return tennisarticle;
+        }
+    }
 }
