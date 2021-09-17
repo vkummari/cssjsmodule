acf7852d67997389336fd7bcf4e49c022e66bf01
Kummari, Venky (NIH/OD) [C] <venky.kummari@nih.gov>
2021-09-16 20:29:31 -0400

    Initial version

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-/** To be started**/
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("Hello - Page loaded")
+    buildNavigationFromArticles();
+});
+
+/**
+ * 
+ */
+function buildNavigationFromArticles() {
+    // Get all the articles on the page
+    const tennisarticles = document.querySelectorAll("article");
+    for (const tennisarticle of tennisarticles) {
+        console.log(tennisarticle.querySelector("h2").innerText);
+        const tennisarticleheader = tennisarticle.querySelector("h2").innerText;
+    }
+
+}
